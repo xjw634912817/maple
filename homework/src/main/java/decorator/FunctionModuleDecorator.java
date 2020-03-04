25c2e4319e75fb6202416dc553fcc94ece1ee20b
xjw634912817 <634912817@qq.com>
2020-03-04 22:38:04 +0800

    装饰器模式：提交作业

--- homework/src/main/java/decorator/FunctionModuleDecorator.java
+++ homework/src/main/java/decorator/FunctionModuleDecorator.java
@@ -1,9 +1,16 @@
 package decorator;
 
-public abstract class FunctionModuleDecorator extends FunctionModule {
+import java.util.List;
+
+public class FunctionModuleDecorator extends FunctionModule {
     protected FunctionModule functionModule;
 
     public FunctionModuleDecorator(FunctionModule functionModule) {
         this.functionModule = functionModule;
+    }
+
+    @Override
+    List<String> getFunctionModules() {
+        return functionModule.getFunctionModules();
     }
 }
