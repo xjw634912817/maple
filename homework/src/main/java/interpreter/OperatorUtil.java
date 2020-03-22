494b425595014ac366e340a072b2bdbdd83d2455
xjw634912817 <634912817@qq.com>
2020-03-22 19:40:35 +0800

    作业：解释器模式 - bug修复， 用两个栈完成

--- homework/src/main/java/interpreter/OperatorUtil.java
+++ homework/src/main/java/interpreter/OperatorUtil.java
@@ -1,33 +1,53 @@
 package interpreter;
 
-import interpreter.interpreter.*;
+
+import interpreter.interpreter.AddInterpreter;
+import interpreter.interpreter.DivideInterpreter;
+import interpreter.interpreter.MultiplyInterpreter;
+import interpreter.interpreter.SubtractInterpreter;
+
+import java.util.Objects;
+import java.util.Stack;
 
 public class OperatorUtil {
-    public static boolean isOperator(String symbol) {
-        return "+".equals(symbol) || "-".equals(symbol)
-                || "*".equals(symbol) || "/".equals(symbol);
+    public static boolean isOperator(String operator) {
+        OperatorEnum[] values = OperatorEnum.values();
+        for (OperatorEnum value : values) {
+            if (Objects.equals(operator, value.getOperator())) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static boolean isLeftBracket(String symbol) {
-        return "(".equals(symbol);
+    public static OperatorEnum getOperatorEnumByOperator(String operator) {
+        OperatorEnum[] values = OperatorEnum.values();
+        for (OperatorEnum value : values) {
+            if (Objects.equals(operator, value.getOperator())) {
+                return value;
+            }
+        }
+        return null;
     }
 
-    public static boolean isRightBracket(String symbol) {
-        return ")".equals(symbol);
-    }
-
-    public static Interpreter getInterpreter(IArithmeticInterpreter left, IArithmeticInterpreter right, String symbol) {
-        switch (symbol) {
-            case "+":
-                return new AddInterpreter(left, right);
-            case "-":
-                return new SubtractInterpreter(left, right);
-            case "*":
-                return new MultiplyInterpreter(left, right);
-            case "/":
-                return new DivideInterpreter(left, right);
+    public static void calculate(Stack<IArithmeticInterpreter> numberStack, Stack<OperatorEnum> operatorStack) {
+        IArithmeticInterpreter right = numberStack.pop();
+        IArithmeticInterpreter left = numberStack.pop();
+        OperatorEnum operatorEnum = operatorStack.pop();
+        switch (operatorEnum) {
+            case ADD:
+                numberStack.push(new AddInterpreter(left, right));
+                return;
+            case SUBTRACT:
+                numberStack.push(new SubtractInterpreter(left, right));
+                return;
+            case MULTIPLY:
+                numberStack.push(new MultiplyInterpreter(left, right));
+                return;
+            case DIVIDE:
+                numberStack.push(new DivideInterpreter(left, right));
+                return;
             default:
-                return null;
         }
     }
 
