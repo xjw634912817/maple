f1e2f0bff43689a1e76a0588b8d3c5c7b1a44583
jeremy.xie <jeremy.xie@ringcentral.com>
2021-11-05 14:41:43 +0800

    update comment

--- springboot-demo/src/main/java/com/maple/springboot/springbootdemo/qualify/QualifyTest.java
+++ springboot-demo/src/main/java/com/maple/springboot/springbootdemo/qualify/QualifyTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class QualifyTest {
 
     /**
-     * Import注解可用于导入其他类（可导入配置类）
+     * Qualifier注解Collection时，会找对应有Qualifier注解的bean来注入
      */
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(QualifyConfiguration.class);
